82bef5de8dd4c1f51e1c2d2dd8706fd22e5b5be7
priya rathore <rathorepriya1705@gmail.com>
2023-06-12 01:17:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import RegisterPage from "./Pages/Register";
 import LoginPage from "./Pages/Login";
+import Navbar from "./Components/Navbar";
+import ListIngPage from "./Pages/Lists";
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/register" element={< RegisterPage />} />
-      <Route path="/Login" element={< LoginPage />} />
-    </Routes>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/register" element={< RegisterPage />} />
+        <Route path="/Login" element={< LoginPage />} />
+        <Route path="/Book/Lists" element={< ListIngPage />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/Context/Firebase-context.jsx
+++ src/Context/Firebase-context.jsx
@@ -8,6 +8,8 @@ import {
   GoogleAuthProvider,
   onAuthStateChanged,
 } from 'firebase/auth';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
+import { getStorage, ref, uploadBytes } from 'firebase/storage';
 
 const FirebaseContext = createContext(null);
 const firebaseConfig = {
@@ -22,9 +24,17 @@ export const useFirebase = () => useContext(FirebaseContext);
 const firebaseapp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseapp);
 const GoogleProvider = new GoogleAuthProvider();
+const Firestore = getFirestore();
+const storage = getStorage();
 
 export const FirebaseProvider = (props) => {
   const [User, setUser] = useState(null);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) setUser(User);
+      else setUser(null);
+    });
+  }, []);
   const signupUserWithEmailAndPassword = (email, password) => {
     createUserWithEmailAndPassword(auth, email, password);
   };
@@ -34,20 +44,38 @@ export const FirebaseProvider = (props) => {
   const signinWithGoogle = () => {
     signInWithPopup(auth, GoogleProvider);
   };
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) setUser(User);
-      else setUser(null);
+  console.log(User);
+  const HandleCreateNewListing = async (
+    Name,
+    isbnNumber,
+    Price,
+    coverPhoto
+  ) => {
+    const imageRef = ref(
+      storage,
+      `Uploads/images/${Date.now()}-${coverPhoto.Name}`
+    );
+    const uploadImage = await uploadBytes(imageRef, coverPhoto);
+    await addDoc(collection(Firestore, 'Books'), {
+      Name,
+      isbnNumber,
+      Price,
+      imageURL: uploadImage.ref.fullPath,
+      userId: User.uid,
+      userEmail: User.email,
+      displayName: User.displayName,
+      photoURL: User.photoURL,
     });
-  }, []);
-  const isLoggedIn = User ? true : false;
+  };
 
+  const isLoggedIn = User ? true : false;
   return (
     <FirebaseContext.Provider
       value={{
         signupUserWithEmailAndPassword,
         LoginUserWithEmailAndPassword,
         signinWithGoogle,
+        HandleCreateNewListing,
         isLoggedIn,
       }}
     >
